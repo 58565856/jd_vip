82708131c6eb0f1bb732d46270bca4d563a08008
Leon <LeoninLyon@gmail.com>
2021-09-10 12:31:33 +0800

    ☘Update.sh

--- Scripts/sh/Update.sh
+++ Scripts/sh/Update.sh
@@ -5,85 +5,93 @@ cron: 32 6,18 * * *
 new Env('自用更新');
 COMMENT
 
-file_db=/ql/db/env.db
-dir_config=/ql/config
-dir_scripts=/ql/scripts
-dir_raw=/ql/raw
-config_raw_path=$dir_raw/config.sh
-config_config_path=$dir_config/config.sh
-extra_raw_path=$dir_raw/extra.sh
-extra_config_path=$dir_config/extra.sh
-code_raw_path=$dir_raw/code.sh
-code_config_path=$dir_config/code.sh
-task_before_raw_path=$dir_raw/task_before.sh
-task_before_config_path=$dir_config/task_before.sh
+## 导入通用变量与函数
+dir_shell=/ql/shell
+. $dir_shell/share.sh
+
+file_db_env=/ql/db/env.db
+file_raw_config=$dir_raw/config.sh
+file_config_config=$dir_config/config.sh
+file_raw_extra=$dir_raw/extra.sh
+file_config_extra=$dir_config/extra.sh
+file_raw_code=$dir_raw/code.sh
+file_config_code=$dir_config/code.sh
+file_raw_task_before=$dir_raw/task_before.sh
+file_config_task_before=$dir_config/task_before.sh
+file_config_notify_js=$dir_config/sendNotify.js
 
 GithubProxyUrl=""
 TG_BOT_TOKEN=""
 TG_USER_ID=""
-openCardBean="10"
+openCardBean=""
 
 CollectedRepo="4"
 OtherRepo=""
-Ninja="down"
+Ninja="on"
 
-repoNum="4"
+repoNum="6"
 HelpType="HelpType=\"0\""
 BreakHelpType="BreakHelpType=\"1\""
 BreakHelpNum="BreakHelpNum=\"31-1000\""
 
 
 update_config() {
-    curl -sL https://git.io/config.sh > $config_raw_path
-    mv -b $config_raw_path $dir_config
-    sed -ri "s/GithubProxyUrl=\"https\:\/\/ghproxy.com\/\"/GithubProxyUrl=\"${GithubProxyUrl}\"/g" $config_config_path
-    sed -i "s/TG_BOT_TOKEN=\"\"/TG_BOT_TOKEN=\"${TG_BOT_TOKEN}\"/g" $config_config_path
-    sed -i "s/TG_USER_ID=\"\"/TG_USER_ID=\"${TG_USER_ID}\"/g" $config_config_path
-    sed -i "s/openCardBean=\"30\"/openCardBean=\"${openCardBean}\"/g" $config_config_path
+    curl -sL https://git.io/config.sh > $file_raw_config
+    mv -b $file_raw_config $dir_config
+    sed -ri "s/GithubProxyUrl=\"https\:\/\/ghproxy.com\/\"/GithubProxyUrl=\"${GithubProxyUrl}\"/g" $file_config_config
+    sed -i "s/TG_BOT_TOKEN=\"\"/TG_BOT_TOKEN=\"${TG_BOT_TOKEN}\"/g" $file_config_config
+    sed -i "s/TG_USER_ID=\"\"/TG_USER_ID=\"${TG_USER_ID}\"/g" $file_config_config
+    sed -i "s/openCardBean=\"30\"/openCardBean=\"${openCardBean}\"/g" $file_config_config
 }
 
 update_extra() {
-    curl -sL https://git.io/extra.sh > $extra_raw_path
-    mv -b $extra_raw_path $dir_config
-    sed -i "s/CollectedRepo=(4)/CollectedRepo=(${CollectedRepo})/g" $extra_config_path
-    sed -i "s/OtherRepo=()/OtherRepo=(${OtherRepo})/g" $extra_config_path
-    sed -i "s/Ninja=\"up\"/Ninja=\"${Ninja}\"/" $extra_config_path
+    curl -sL https://git.io/extra.sh > $file_raw_extra
+    mv -b $file_raw_extra $dir_config
+    sed -i "s/CollectedRepo=(4)/CollectedRepo=(${CollectedRepo})/g" $file_config_extra
+    sed -i "s/OtherRepo=()/OtherRepo=(${OtherRepo})/g" $file_config_extra
+    sed -i "s/Ninja=\"on\"/Ninja=\"${Ninja}\"/" $file_config_extra
+    sed -i '/NOWTIME=/a\\tcp \/ql\/config\/sendNotify.js \/ql\/scripts\/sendNotify.js' $file_config_extra
+    echo 'cat /ql/db/wskey.list >> /ql/config/wskey.list && :> /ql/db/wskey.list' >> $file_config_extra
 }
 
 update_code() {
-    curl -sL https://git.io/code.sh > $code_raw_path
-    mv -b $code_raw_path $dir_config
-    sed -i "s/repo=\$repo4/repo=\$repo${repoNum}/g" $code_config_path
-    sed -i "/^HelpType=/c${HelpType}" $code_config_path
-    sed -i "/^BreakHelpType=/c${BreakHelpType}" $code_config_path
-    sed -i "/^BreakHelpNum=/c${BreakHelpNum}" $code_config_path
+    curl -sL https://git.io/code.sh > $file_raw_code
+    mv -b $file_raw_code $dir_config
+    sed -i "s/repo=\$repo4/repo=\$repo${repoNum}/g" $file_config_code
+    sed -i "/^HelpType=/c${HelpType}" $file_config_code
+    sed -i "/^BreakHelpType=/c${BreakHelpType}" $file_config_code
+    sed -i "/^BreakHelpNum=/c${BreakHelpNum}" $file_config_code
 }
 
 update_task_before() {
-    curl -sL https://git.io/task_before.sh > $task_before_raw_path
-    mv -b $task_before_raw_path $dir_config
+    curl -sL https://git.io/task_before.sh > $file_raw_task_before
+    mv -b $file_raw_task_before $dir_config
 }
 
 random_cookie() {
     c=1000000
     for r in {1..3}; do
         p=`expr $c - $r`
-        sed -ri "s/\"position\"\:${p}/regular${r}/" $file_db
+        sed -ri "s/\"position\"\:${p}/regular${r}/" $file_db_env
     done
     for line in {1..100}; do
-        sed -ri "${line}s/(\"position\"\:)[^,]*/\"position\"\:${RANDOM}/" $file_db
+        sed -ri "${line}s/(\"position\"\:)[^,]*/\"position\"\:${RANDOM}/" $file_db_env
     done
     for r in {1..3}; do
         p=`expr $c - $r`
-        sed -ri "s/regular${r}/\"position\"\:${p}/" $file_db
+        sed -ri "s/regular${r}/\"position\"\:${p}/" $file_db_env
     done
     ql check
+}
+
+update_notify() {
+    wget -O sendNotify.js https://raw.githubusercontent.com/ccwav/QLScript/main/sendNotify.js
+    sed -ri 's/\\n\\n本通知 By ccwav Mod/\\n\\n本通知 By Oreo Mod/' $file_config_notify_js
 }
 
 update_ninja() {
     cp -rf /ql/config/sendNotify.js /ql/scripts/sendNotify.js
     cp -rf /ql/config/sendNotify.js /ql/ninja/backend/sendNotify.js
-    wget -q https://raw.githubusercontent.com/Oreomeow/VIP/main/Scripts/html/index.html -O /ql/config/index.html
     cp -rf /ql/config/index.html /ql/ninja/backend/static/index.html
     cd /ql/ninja/backend && pm2 start
 }
@@ -92,12 +100,14 @@ update_ninja() {
 update_config
 update_extra
 update_code
-# update_task_before
-update_ninja
+update_task_before
 
 if [ $(date "+%H") -eq 18 ]; then
     random_cookie
 fi
+
+update_notify
+update_ninja
 
 case $@ in
     ck)
