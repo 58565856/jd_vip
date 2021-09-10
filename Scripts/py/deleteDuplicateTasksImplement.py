844dc944f090b38dcd31cf3c63909290e6de463a
Leon <LeoninLyon@gmail.com>
2021-09-10 08:37:00 +0800

    🔨format

--- Scripts/py/deleteDuplicateTasksImplement.py
+++ Scripts/py/deleteDuplicateTasksImplement.py
@@ -9,7 +9,7 @@ import os, sys
 import requests
 import time
 
-ip="localhost"
+ip = "localhost"
 
 def loadSend():
     print("加载推送功能")
@@ -23,17 +23,17 @@ def loadSend():
             print("加载通知服务失败~")
 
 headers={
-    "Accept":        "application/json",
+    "Accept": "application/json",
     "Authorization": "Basic YWRtaW46YWRtaW4=",
-    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
 }
 
 def getTaskList():
     t = round(time.time() * 1000)
     url = "http://%s:5700/api/crons?searchValue=&t=%d" % (ip, t)
     response = requests.get(url=url, headers=headers)
-    responseContent=json.loads(response.content.decode('utf-8'))
-    if responseContent['code']==200:
+    responseContent = json.loads(response.content.decode('utf-8'))
+    if responseContent['code'] == 200:
         taskList= responseContent['data']
         return taskList
     else:
@@ -42,8 +42,8 @@ def getTaskList():
 
 
 def getDuplicate(taskList):
-    wholeNames={}
-    duplicateID=[]
+    wholeNames = {}
+    duplicateID = []
     for task in taskList:
         if task['name'] in wholeNames.keys():
             duplicateID.append(task['_id'])
@@ -54,23 +54,23 @@ def getDuplicate(taskList):
 
 def getData(duplicateID):
     rawData = "["
-    count=0
+    count = 0
     for id in duplicateID:
         rawData += "\"%s\""%id
-        if count<len(duplicateID)-1:
+        if count < len(duplicateID) - 1:
             rawData += ", "
-        count+=1
+        count += 1
     rawData += "]"
     return rawData
 
 def deleteDuplicateTasks(duplicateID):
     t = round(time.time() * 1000)
     url = "http://%s:5700/api/crons?t=%d" % (ip, t)
-    data=json.dumps(duplicateID)
-    headers["Content-Type"]="application/json;charset=UTF-8"
-    response=requests.delete(url=url,headers=headers,data=data)
+    data = json.dumps(duplicateID)
+    headers["Content-Type"] = "application/json;charset=UTF-8"
+    response = requests.delete(url=url, headers=headers, data=data)
     msg = json.loads(response.content.decode('utf-8'))
-    if msg['code']!=200:
+    if msg['code'] != 200:
         print("出错！，错误信息为：%s"%msg)
     else:
         print("成功删除重复任务")
@@ -79,8 +79,8 @@ def loadToken():
     # cur_path = os.path.abspath(os.path.dirname(__file__))
     # send("当前路径：",cur_path)
     try:
-        with open("/ql/config/auth.json","r",encoding="utf-8") as f:
-            data=json.load(f)
+        with open("/ql/config/auth.json", "r", encoding="utf-8") as f:
+            data = json.load(f)
     except:
         # pass
         send("无法获取token","")
